068c69afe3d03877b04fba67a82c03943d38bd60
Andre Weber <andre.weber3@etas.com>
2024-11-15 13:14:14 +0100

    Align Java and Kotlin JavaVersion
    Increased Sample App JavaVersion from Java 8 to
    Java 11 because Java 11 is slowly fading out.

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -22,8 +22,14 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
 }
 
 protobuf {
